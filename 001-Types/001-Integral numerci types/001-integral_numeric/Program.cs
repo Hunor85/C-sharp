803589d6457830176b38e454a5ab7c03de6ec738
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-09-27 11:06:51 +0200

    init

---
+++ 001-Types/001-Integral numerci types/001-integral_numeric/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _001_integral_numeric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Min and Max Value for SByte
+            Console.WriteLine("---------- SByte ----------");
+            sbyte sbyteMinValue = sbyte.MinValue;
+            sbyte sbyteMaxValue = sbyte.MaxValue;
+            Console.WriteLine("The Minimum value for SByte is {0}: ", sbyteMinValue);
+            Console.WriteLine("The Maximum value for SByte is {0}: ", sbyteMaxValue);
+
+            // Min and Max Value for Byte
+            Console.WriteLine("---------- Byte ----------");
+            byte byteMinValue = byte.MinValue;
+            byte byteMaxValue = byte.MaxValue;
+            Console.WriteLine("The Minimum value for Byte is {0}: ", byteMinValue);
+            Console.WriteLine("The Maximum value for Byte is {0}: ", byteMaxValue);
+
+            // Min and Max Value for Short
+            Console.WriteLine("---------- Short ----------");
+            short shortMinValue = short.MinValue;
+            short shortMaxValue = short.MaxValue;
+            Console.WriteLine("The Minimum value for Short is {0}: ", shortMinValue);
+            Console.WriteLine("The Maximum value for Short is {0}: ", shortMaxValue);
+
+            // Min and Max Value for UShort
+            Console.WriteLine("---------- UShort ----------");
+            ushort ushortMinValue = ushort.MinValue;
+            ushort ushortMaxValue = ushort.MaxValue;
+            Console.WriteLine("The Minimum value for UShort is {0}: ", ushortMinValue);
+            Console.WriteLine("The Maximum value for UShort is {0}: ", ushortMaxValue);
+
+            // Min and Max Value for Int
+            Console.WriteLine("---------- Int ----------");
+            int intMinValue = int.MinValue;
+            int intMaxValue = int.MaxValue;
+            Console.WriteLine("The Minimum value for Int is {0}: ", intMinValue);
+            Console.WriteLine("The Maximum value for Int is {0}: ", intMaxValue);
+
+            // Min and Max Value for UInt
+            Console.WriteLine("---------- UInt ----------");
+            uint uintMinValue = uint.MinValue;
+            uint uintMaxValue = uint.MaxValue;
+            Console.WriteLine("The Minimum value for UInt is {0}: ", uintMinValue);
+            Console.WriteLine("The Maximum value for UInt is {0}: ", uintMaxValue);
+
+            // Min and Max Value for Long
+            Console.WriteLine("---------- Long ----------");
+            long longMinValue = long.MinValue;
+            long longMaxValue = long.MaxValue;
+            Console.WriteLine("The Minimum value for Long is {0}: ", longMinValue);
+            Console.WriteLine("The Maximum value for Long is {0}: ", longMaxValue);
+
+            // Min and Max Value for ULong
+            Console.WriteLine("---------- ULong ----------");
+            ulong ulongMinValue = ulong.MinValue;
+            ulong ulongMaxValue = ulong.MaxValue;
+            Console.WriteLine("The Minimum value for ULong is {0}: ", ulongMinValue);
+            Console.WriteLine("The Maximum value for ULong is {0}: ", ulongMaxValue);
+        }
+    }
+}
