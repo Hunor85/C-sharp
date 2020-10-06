22feba7f2b8abcd68e3bfd20b03f4415fe12bb5b
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-10-06 10:55:43 +0200

    init

---
+++ 001-Types/003-Implicit conversions/002-byte/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+namespace _002_byte
+{
+    class Program
+    {
+        /* 
+        * Implicit conversion 
+        * From byte
+        * To short, ushort, int, uint, long, ulong, float, double and decimal
+        */
+        static void Main(string[] args)
+        {
+            // Min Value for Byte
+            // Implicit conversion from Byte to Short
+            Console.WriteLine("---------- Byte Min to Short ----------");
+            byte byteMinValueA = byte.MinValue;
+            short shortValueA = byteMinValueA;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueA);
+            Console.WriteLine("The type of shortValeuA: {0}", shortValueA.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to Short
+            Console.WriteLine("---------- Byte Max to Short ----------");
+            byte byteMaxValueB = byte.MaxValue;
+            short shortValueB = byteMaxValueB;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueB);
+            Console.WriteLine("The type of shortValeuB: {0}", shortValueB.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to UShort
+            Console.WriteLine("---------- Byte Min to UShort ----------");
+            byte byteMinValueC = byte.MinValue;
+            ushort shortValueC = byteMinValueC;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueC);
+            Console.WriteLine("The type of shortValeuC: {0}", shortValueC.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to UShort
+            Console.WriteLine("---------- Byte Max to UShort ----------");
+            byte byteMinValueD = byte.MinValue;
+            ushort shortValueD = byteMinValueD;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueD);
+            Console.WriteLine("The type of shortValeuD: {0}", shortValueD.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to Int
+            Console.WriteLine("---------- Byte Min to Int ----------");
+            byte byteMinValueE = byte.MinValue;
+            int shortValueE = byteMinValueE;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueE);
+            Console.WriteLine("The type of shortValeuE: {0}", shortValueE.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to Int
+            Console.WriteLine("---------- Byte Max to Int ----------");
+            byte byteMaxValueF = byte.MaxValue;
+            int shortValueF = byteMaxValueF;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueF);
+            Console.WriteLine("The type of shortValeuF: {0}", shortValueF.GetTypeCode());    
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to UInt
+            Console.WriteLine("---------- Byte Min to UInt ----------");
+            byte byteMinValueG = byte.MinValue;
+            uint shortValueG = byteMinValueG;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueG);
+            Console.WriteLine("The type of shortValeuG: {0}", shortValueG.GetTypeCode());    
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to UInt
+            Console.WriteLine("---------- Byte Max to UInt ----------");
+            byte byteMaxValueH = byte.MaxValue;
+            uint shortValueH = byteMaxValueH;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueH);
+            Console.WriteLine("The type of shortValeuH: {0}", shortValueH.GetTypeCode());    
+
+            Console.WriteLine("\n");
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to Long
+            Console.WriteLine("---------- Byte Min to Long ----------");
+            byte byteMinValueI = byte.MinValue;
+            long shortValueI = byteMinValueI;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueI);
+            Console.WriteLine("The type of shortValeuI: {0}", shortValueI.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to Long
+            Console.WriteLine("---------- Byte Max to Long ----------");
+            byte byteMaxValueJ = byte.MaxValue;
+            long shortValueJ = byteMaxValueJ;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueJ);
+            Console.WriteLine("The type of shortValeuJ: {0}", shortValueJ.GetTypeCode());    
+
+            Console.WriteLine("\n");            
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to ULong
+            Console.WriteLine("---------- Byte Min to ULong ----------");
+            byte byteMinValueK = byte.MinValue;
+            ulong shortValueK = byteMinValueK;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueK);
+            Console.WriteLine("The type of shortValeuK: {0}", shortValueK.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to ULong
+            Console.WriteLine("---------- Byte Max to ULong ----------");
+            byte byteMaxValueL = byte.MaxValue;
+            ulong shortValueL = byteMaxValueL;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueL);
+            Console.WriteLine("The type of shortValeuL: {0}", shortValueL.GetTypeCode());    
+
+            Console.WriteLine("\n"); 
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to Float
+            Console.WriteLine("---------- Byte Min to Float ----------");
+            byte byteMinValueM = byte.MinValue;
+            Single shortValueM = byteMinValueM;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueM);
+            Console.WriteLine("The type of shortValeuM: {0}", shortValueM.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to Float
+            Console.WriteLine("---------- Byte Max to Float ----------");
+            byte byteMaxValueN = byte.MaxValue;
+            Single shortValueN = byteMaxValueN;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueN);
+            Console.WriteLine("The type of shortValeuN: {0}", shortValueN.GetTypeCode());    
+
+            Console.WriteLine("\n");   
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to Double
+            Console.WriteLine("---------- Byte Min to Double ----------");
+            byte byteMinValueO = byte.MinValue;
+            Double shortValueO = byteMinValueO;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueO);
+            Console.WriteLine("The type of shortValeuO: {0}", shortValueO.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to Double
+            Console.WriteLine("---------- Byte Max to Double ----------");
+            byte byteMaxValueP = byte.MaxValue;
+            Double shortValueP = byteMaxValueP;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueP);
+            Console.WriteLine("The type of shortValeuP: {0}", shortValueP.GetTypeCode());    
+
+            Console.WriteLine("\n");  
+
+            // Min Value for Byte
+            // Implicit conversion from Byte to Decimal
+            Console.WriteLine("---------- Byte Min to Decimal ----------");
+            byte byteMinValueQ = byte.MinValue;
+            Decimal shortValueQ = byteMinValueQ;
+            Console.WriteLine("The minimum value for Byte: {0}", byteMinValueQ);
+            Console.WriteLine("The type of shortValeuQ: {0}", shortValueQ.GetTypeCode());
+
+            Console.WriteLine("\n");
+
+            // Max Value for Byte
+            // Implicit conversion from Byte to Decimal
+            Console.WriteLine("---------- Byte Max to Decimal ----------");
+            byte byteMaxValueR = byte.MaxValue;
+            Decimal shortValueR = byteMaxValueR;
+            Console.WriteLine("The maximum value for Byte: {0}", byteMaxValueR);
+            Console.WriteLine("The type of shortValeuR: {0}", shortValueR.GetTypeCode());    
+
+            Console.WriteLine("\n"); 
+        }
+    }
+}
