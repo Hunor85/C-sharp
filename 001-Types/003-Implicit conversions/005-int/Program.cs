7140cc0be54bbac1957b58fe019677b7cd3ecd50
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-10-08 13:36:18 +0200

    init

---
+++ 001-Types/003-Implicit conversions/005-int/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _005_int
+{
+    class Program
+    {
+        /* 
+        * Implicit conversion 
+        * From int
+        * To long, float, double, or decimal
+        */
+        static void Main(string[] args)
+        {
+            int val1 = Int32.MinValue;
+            int val2 = Int32.MaxValue;
+
+            long val3 = val1;
+            Console.WriteLine(val3.GetType());
+
+            long val4 = val2;
+            Console.WriteLine(val4.GetType());
+
+            float v5 = val1;
+            Console.WriteLine(v5.GetType());
+
+            float v6 = val2;
+            Console.WriteLine(v6.GetType());
+
+            double v7 = val1;
+            Console.WriteLine(v7.GetType());
+
+            double v8 = val2;
+            Console.WriteLine(v8.GetType());
+
+            decimal v9 = val1;
+            Console.WriteLine(v9.GetType());
+
+            decimal v10 = val2;
+            Console.WriteLine(v10.GetType());
+        }
+    }
+}
