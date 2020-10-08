86402514fef7d6037eb4fa5a4cf1ee71a8f70e02
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-10-08 19:17:34 +0200

    init

---
+++ 001-Types/003-Implicit conversions/008-ulong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _008_ulong
+{
+    class Program
+    {
+        /* 
+        * Implicit conversion 
+        * From ulong
+        * To float, double, or decimal
+        */
+        static void Main(string[] args)
+        {
+            ulong val1 = ulong.MinValue;
+            ulong val2 = ulong.MaxValue;
+
+            float v3 = val1;
+            Console.WriteLine(v3.GetType());
+
+            float v4 = val2;
+            Console.WriteLine(v4.GetType());
+
+            double v5 = val1;
+            Console.WriteLine(v5.GetType());
+
+            double v6 = val2;
+            Console.WriteLine(v6.GetType());
+
+            decimal v7 = val1;
+            Console.WriteLine(v7.GetType());
+
+            decimal v8 = val2;
+            Console.WriteLine(v8.GetType());        
+        }
+    }
+}
---
+++ 001-Types/003-Implicit conversions/009-float/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _009_float
+{
+    class Program
+    {
+        /* 
+        * Implicit conversion 
+        * From float
+        * To double
+        */
+        static void Main(string[] args)
+        {
+            float v1 = float.MinValue;
+            float v2 = float.MaxValue;
+
+            double v3 = v1;
+            Console.WriteLine(v3.GetType());
+
+            double v4 = v2;
+            Console.WriteLine(v4.GetType());
+        }
+    }
+}
