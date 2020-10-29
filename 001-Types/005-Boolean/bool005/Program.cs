00a00c9d9a61dd993aaf56a8d52d8fe3ccc920b0
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-10-29 13:45:49 +0100

    init

---
+++ 001-Types/005-Boolean/bool005/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace bool005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output: True
+            bool v1 = true || true;
+            Console.WriteLine(v1);
+
+            Console.WriteLine("\n");
+
+            // Output: True
+            bool v2 = true || false;
+            Console.WriteLine(v2);
+
+            Console.WriteLine("\n");
+
+            // Output: False
+            bool v3 = false || false;
+            Console.WriteLine(v3);
+
+            Console.WriteLine("\n");
+
+            // Output: True
+            bool v4 = false || true;
+            Console.WriteLine(v4);
+            
+            Console.WriteLine("\n");
+        }
+    }
+}
