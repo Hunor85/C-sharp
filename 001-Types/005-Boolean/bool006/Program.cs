15f0930e0ef04804414c2aabd8c529acf53b6c77
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-11-03 11:30:25 +0100

    init

---
+++ 001-Types/005-Boolean/bool006/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace bool006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool? n = null;
+
+            bool? v1 = true ^ null;
+            Console.WriteLine(v1);
+            Console.WriteLine(v1 is null ? "null" : v1.Value.ToString());
+
+            bool? v2 = null ^ true;
+            Console.WriteLine(v2);
+            Console.WriteLine(v2 is null ? "null" : v2.Value.ToString());
+
+
+            bool? v3 = false ^ null;
+            Console.WriteLine(v3);
+            Console.WriteLine(v3 is null ? "null" : v3.Value.ToString());
+
+
+            bool? v4 = null ^ false;
+            Console.WriteLine(v4);
+            Console.WriteLine(v4 is null ? "null" : v4.Value.ToString());
+
+        }
+    }
+}
