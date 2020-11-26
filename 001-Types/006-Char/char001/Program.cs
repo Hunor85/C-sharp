63afcb6584c94cf1fa7f1e412cb719f5ce6caaad
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-11-26 11:35:38 +0100

    make new char solution

---
+++ 001-Types/006-Char/char001/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace char001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // https://www.geeksforgeeks.org/c-sharp-char-struct/ => Char.IsLetter(String, Int32) Method
+        }
+    }
+}
